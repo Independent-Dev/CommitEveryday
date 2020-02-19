4b24c6ee207bc6a81caa3f3e7e45e62d9ded058e
jonathan <juju08217@daum.net>
2020-02-19 20:55:20 +0900

    jquery ui practice from yesterday commit

---
+++ Jquery/js/jqueryUI.js
@@ -0,0 +1,5 @@
+$('#datepicker').datepicker({changeMonth:true, changeYear:true});
+
+$('#type_select').buttonset();
+
+$('#slide_height').slider();
--- Jquery/js/signature.js
+++ Jquery/js/signature.js
@@ -2,25 +2,33 @@ var canvas = document.querySelector("#signature");
 
 var signaturePad = new SignaturePad(canvas);
 
+const download = document.querySelector('#download');
 const save = document.querySelector('#save');
 const clear = document.querySelector('#clear');
-const input = document.querySelector('#fileName')
+const input = document.querySelector('#fileName');
+const dataURL = document.querySelector('#dataURL');
+const fileName = document.querySelector('#fileName');
+const form = document.querySelector('#form');
+
 input.placeholder = "file name";
 
 //https://stackoverflow.com/questions/8126623/downloading-canvas-element-to-an-image
 //data를 다운로드하는 다른 방법은 없을까...
-var download = function(){
+var downloadPNG = function(){
     if(signaturePad.isEmpty()){
         alert("먼저 사인을 하세요!!")
     }else{
     var link = document.createElement('a');
-    fileName = input.value;
-    if(fileName == ""){
+    var filename = input.value;
+    if(filename == ""){
         link.download = 'signature.png';
     }else{
-        link.download = fileName+".png";
+        link.download = filename+".png";
     }
     link.href = document.getElementById('signature').toDataURL()    
+    // input.value = link.href;
+    // $('#saveForm').val(link.href);
+    // console.log($('#saveForm').val());
     link.click();
     // link.text = "aaaaa";
     // document.getElementsByTagName('body')[0].appendChild(link);
@@ -31,9 +39,37 @@ function clearHandler(event){
     signaturePad.clear();
 }
 
-save.addEventListener("click", download);
+function saveHandler(event){
+    dataURL.value = document.getElementById('signature').toDataURL();
+    var filename = "";
+    if(input.value == ""){
+        filename = 'signature.png';
+    }else{
+        filename = input.value+".png";
+    } 
+    $.ajax({ url: "/rest/1/pages/245", // 클라이언트가 HTTP 요청을 보낼 서버의 URL 주소 
+    data: { 
+        filename: filename,
+        dataurl :document.getElementById('signature').toDataURL()
+     }, // HTTP 요청과 함께 서버로 보낼 데이터 
+    method: "GET", // HTTP 요청 메소드(GET, POST 등) 
+    dataType: "json" // 서버에서 보내줄 데이터의 타입 
+    })
+    console.log(document.getElementById('signature').toDataURL());
+
+    // fileName.value = filename;
+    // console.log(fileName.value)
+    // form.submit();
+
+}
+
+download.addEventListener("click", downloadPNG);
 
 clear.addEventListener("click", clearHandler);
+
+save.addEventListener('click', saveHandler);
+
+
 
 // function saveHandler(event){
 //     console.log(1);
